dc64ad3714eed4095cff270fa91088a21e0648fb
hughjfchen <hugh.jf.chen@gmail.com>
2022-05-18 06:26:34 +0800

    minor fixes.

--- db-gw/config/test/testvm1/postgrest.nix
+++ db-gw/config/test/testvm1/postgrest.nix
@@ -1,13 +1,13 @@
 { pkgs, lib, config, ... }:
 let inherit pkgs lib config;
 in {
-  "db-uri" =
+  "db-uri" = ''
     "postgres://${config.my-db-config.db.user}:${config.my-db-config.db.password}@${config.my-db-config.db.host}:${
       toString config.my-db-config.db.port
-    }/${config.my-db-config.db.database}";
-  "db-anon-role" = "${config.my-db-config.db.anonymousRole}";
-  "db-schema" = "${config.my-db-config.db.schema}";
-  "jwt-secret" = "${config.my-db-config.db.jwtSecret}";
-  "server-host" = "testvm1";
+    }/${config.my-db-config.db.database}"'';
+  "db-anon-role" = ''"${config.my-db-config.db.anonymousRole}"'';
+  "db-schema" = ''"${config.my-db-config.db.schema}"'';
+  "jwt-secret" = ''"${config.my-db-config.db.jwtSecret}"'';
+  "server-host" = ''"testvm1"'';
   "server-port" = 3000;
 }
