7767d854d6f757b02d49a58fd168fcf303586786
hughjfchen <hugh.jf.chen@gmail.com>
2022-06-03 16:39:48 +0800

    refactor to release tools based on nix.

--- db/release.nix
+++ db/release.nix
@@ -52,7 +52,7 @@ let
     };
   })).config.systemd.units."postgresql.service".unit;
 
-in {
+in rec {
   inherit nativePkgs pkgs my-db-config;
 
   mk-my-postgresql-service-systemd-setup-or-bin-sh = if my-db-env.db.isSystemdService then
@@ -65,4 +65,106 @@ in {
     })
   else
     { };
+  mk-my-postgresql-reference = nPkgs.writeReferencesToFile mk-my-postgresql-service-systemd-setup-or-bin-sh;
+  mk-my-postgresql-deploy-sh = nPkgs.writeScriptApplication {
+    name = "mk-my-postgresql-deploy-sh";
+    runtimeInputs = [];
+    text = ''
+    # this script need to be run with root or having sudo permission
+    [ $EUID -ne 0 ] && ! sudo -v >/dev/null 2>&1 && echo "need to run with root or sudo" && exit 127
+
+    # some command fix up for systemd service, especially web server
+    getent group nogroup > /dev/null || sudo groupadd nogroup
+
+    # create user and group
+    getent group "${my-db-env.db.processUser}" > /dev/null || sudo groupadd "${my-db-env.db.processUser}"
+    getent passwd "${my-db-env.db.processUser}" > /dev/null || sudo useradd -m -p Passw0rd -g "${my-db-env.db.processUser}" "${my-db-env.db.processUser}"
+
+    # create directories
+    for dirToMk in "${my-db-env.db.configDir}" "${my-db-env.db.runDir}" "${my-db-env.db.dataDir}"
+    do
+      if [ ! -d "$dirToMk" ]; then
+         sudo mkdir -p "$dirToMk"
+         sudo chown -R "${my-db-env.db.processUser}":"${my-db-env.db.processUser}" "$dirToMk"
+      fi
+    done
+
+    # now unpack(note we should preserve the /nix/store directory structure)
+    sudo tar zPxf ./my-postgresql_dist.tar.gz
+    sudo chown -R "${my-db-env.db.processUser}":"${my-db-env.db.processUser}" /nix
+
+    # setup the systemd service or create a link to the executable
+    if [ "${my-db-env.db.isSystemdService}" == "true" ]; then
+      sudo "${mk-my-postgresql-service-systemd-setup-or-bin-sh}"/bin/setup-systemd-units
+    else
+      ln -s "${mk-my-postgresql-service-systemd-setup-or-bin-sh}"/bin/postgres "${my-db-env.db.runDir}"/postgres
+      echo "To use the program, type ${my-db-env.db.runDir}/postgres at the command prompt."
+    fi
+
+    '';
+  };
+  mk-my-postgresql-cleanup-sh = nPkgs.writeScriptApplication {
+    name = "mk-my-postgresql-cleanup-sh";
+    runtimeInputs = [];
+    text = ''
+    # this script need to be run with root or having sudo permission
+    [ $EUID -ne 0 ] && ! sudo -v >/dev/null 2>&1 && echo "need to run with root or sudo" && exit 127
+
+    # how do we unsetup the systemd unit? we do not unsetup the systemd service for now
+    # we just stop it before doing the cleanup
+    if [ "${my-db-env.db.isSystemdService}" == "true" ]; then
+      sudo systemctl stop my-postgresql.service
+    fi
+
+    # do we need to delete the program and all its dependencies in /nix/store?
+    # we do not do that for now
+    # sudo rm -fr /nix/store/xxx ( maybe a file list for the package and its references )
+
+    for dirToMk in "${my-db-env.db.configDir}" "${my-db-env.db.runDir}" "${my-db-env.db.dataDir}"
+    do
+      if [ ! -d "$dirToMk" ]; then
+         sudo rm -fr "$dirToMk"
+      fi
+    done
+
+    getent passwd "${my-db-env.db.processUser}" > /dev/null && sudo userdel -fr "${my-db-env.db.processUser}"
+    getent group "${my-db-env.db.processUser}" > /dev/null && sudo groupdel -f "${my-db-env.db.processUser}"
+
+    '';
+  };
+  mk-dist-full-pack = nPkgs.writeScriptApplication {
+    name = "mk-dist-full-pack";
+    runtimeInputs = [];
+    text = ''
+      # pack the systemd service or executable sh and dependencies with full path
+      pkg_list_temp=$(mktemp)
+      cp "${mk-my-postgresql-reference}" $pkg_list_temp
+      {
+        echo "${mk-my-postgresql-deploy-sh}"
+        echo "${mk-my-postgresql-cleanup-sh}"
+      } >> $pkg_list_temp
+      tar zPcf ./my-postgresql-full-pack-${site}-${phase}.tar.gz -T $pkg_list_temp
+      rm -fr $pkg_list_temp
+
+      # pack the previous tarball and the two scripts for distribution
+      ln -s "${mk-my-postgresql-deploy-sh}" ./deploy-my-postgresql-to-${site}-${phase}
+      ln -s "${mk-my-postgresql-cleanup-sh}" ./cleanup-my-postgresql-run-env-on-${site}-${phase}
+      tar zcf my-postgresql-full-pack-dist-${site}-${phase}.tar.gz \
+        ./my-postgresql-full-pack-${site}-${phase}.tar.gz \
+        ./deploy-my-postgresql-to-${site}-${phase} \
+        ./cleanup-my-postgresql-run-env-on-${site}-${phase}
+      rm -fr ./my-postgresql-full-pack-${site}-${phase}.tar.gz ./deploy-my-postgresql-to-${site}-${phase} ./cleanup-my-postgresql-run-env-on-${site}-${phase}
+    '';
+  };
+  mk-dist-delta-pack = nPkgs.writeScriptApplication {
+    name = "mk-delta-full-pack";
+    runtimeInputs = [];
+    text = ''
+      tar zPcf my-postgresql-full-pack.tar.gz -T "${mk-my-postgresql-reference}"
+      ln -s "${mk-my-postgresql-deploy-sh}" ./deploy-my-postgresql
+      ln -s "${mk-my-postgresql-cleanup-sh}" ./undeploy-my-postgresql
+      tar zcf my-postgresql-full-pack_dist.tar.gz my-postgresql-full-pack.tar.gz ./deploy-my-postgresql ./undeploy-my-postgresql
+      rm -fr ./deploy-my-postgresql ./undeploy-my-postgresql
+    '';
+  };
 }
