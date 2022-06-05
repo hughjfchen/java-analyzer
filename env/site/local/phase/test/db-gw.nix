c2cd32f3841ee5a80fe8d645e544647664d7b651
hughjfchen <hugh.jf.chen@gmail.com>
2022-06-05 21:27:51 +0800

    add local env and config.

---
+++ env/site/local/phase/test/db-gw.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+let envSubM = import ../../../../env.nix { inherit config lib pkgs; };
+in {
+  imports = [ ];
+
+  options = {
+    db-gw = lib.mkOption {
+      type = lib.types.submodule envSubM;
+      description = ''
+        The deploy target host env.
+      '';
+    };
+  };
+
+  config = {
+    db-gw = rec {
+      hostName = "localhost";
+      dnsName = "localhost";
+      ipAddress = "127.0.0.1";
+      processUser = "jadbgwuser";
+      isSystemdService = true;
+      configDir = "/var/${processUser}/config";
+      runDir = "/var/${processUser}/run";
+      dataDir = "/var/${processUser}/data";
+    };
+  };
+}
---
+++ env/site/local/phase/test/messaging.nix
@@ -0,0 +1,27 @@
+{ config, lib, pkgs, ... }:
+let envSubM = import ../../../../env.nix { inherit config lib pkgs; };
+in {
+  imports = [ ];
+
+  options = {
+    messaging = lib.mkOption {
+      type = lib.types.submodule envSubM;
+      description = ''
+        The deploy target host env.
+      '';
+    };
+  };
+
+  config = {
+    messaging = rec {
+      hostName = "localhost";
+      dnsName = "localhost";
+      ipAddress = "127.0.0.1";
+      processUser = "jamsguser";
+      isSystemdService = true;
+      configDir = "/var/${processUser}/config";
+      runDir = "/var/${processUser}/run";
+      dataDir = "/var/${processUser}/data";
+    };
+  };
+}
