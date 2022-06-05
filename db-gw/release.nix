5891fd55b80962283c10f5e9c9d3aa3df9a0f669
hughjfchen <hugh.jf.chen@gmail.com>
2022-06-05 10:03:04 +0800

    minor fix

--- db-gw/release.nix
+++ db-gw/release.nix
@@ -25,7 +25,7 @@ let
   my-postgrest-config-kv = nPkgs.writeTextFile {
     name = lib.concatStringsSep "-" [ pkgName "config" ];
     # generate the key = value format config, refer to the lib.generators for other formats
-    text = lib.generators.toKeyValue my-postgrest-config.db-gw;
+    text = (lib.generators.toKeyValue {}) my-postgrest-config.db-gw;
   };
 
   # my services dependencies
