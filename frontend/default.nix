f0d6e0005b88657b1ae8fd442cd910e1bc1a4dd2
hughjfchen <hugh.jf.chen@gmail.com>
2022-06-07 12:58:23 +0800

    fix buildcommand.

--- frontend/default.nix
+++ frontend/default.nix
@@ -26,7 +26,7 @@ in rec {
 
   java-analyzer-frontend = pkgs.mkYarnPackage rec {
     pname = "java-analyzer-frontend";
-    version = "0.0.0.1";
+    version = "0.1.0";
     src = ./.;
     packageJSON = ./package.json;
     yarnLock = ./yarn.lock;
@@ -40,7 +40,6 @@ in rec {
       # react-scripts requires a writable node_modules/.cache, so we have to copy the symlink's contents back
       # into `node_modules/`.
       # See https://github.com/facebook/create-react-app/issues/11263
-      cd deps/${pname}
       node_modules="$(readlink node_modules)"
       rm node_modules
       mkdir -p "$WRITABLE_NODE_MODULES"/.cache
@@ -52,14 +51,13 @@ in rec {
         ln -sfv "$node_modules"/.bin/"$(readlink "$x")" "$WRITABLE_NODE_MODULES"/.bin/"$(basename "$x")"
       done
       ln -sfv "$WRITABLE_NODE_MODULES" node_modules
-      cd ../..
       yarn build
-      cd deps/${pname}
       rm -rf node_modules
       ln -sf $node_modules node_modules
-      cd ../..
     '';
+    # for non-bianry package
     distPhase = "true";
+    # following is to fix too-clever config
     configurePhase = "ln -s $node_modules node_modules";
   };
 
