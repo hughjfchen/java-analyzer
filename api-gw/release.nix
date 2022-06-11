e51fc943c5b7b4545834baa2dc2dfcf985571244
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-06-11 23:18:25 +0800

    refactor all release.nix.

--- api-gw/release.nix
+++ api-gw/release.nix
@@ -1,7 +1,7 @@
 { nativePkgs ? import ./default.nix { }, # the native package set
 pkgs ? import ./cross-build.nix { }
 , # the package set for corss build, we're especially interested in the fully static binary
-site , # the site for release, the binary would deploy to it finally
+site, # the site for release, the binary would deploy to it finally
 phase, # the phase for release, must be "local", "test" and "production"
 }:
 let
@@ -9,19 +9,32 @@ let
   sPkgs = pkgs.x86-musl64; # for the fully static build
   lib = nPkgs.lib; # lib functions from the native package set
   pkgName = "my-openresty";
-  innerTarballName = lib.concatStringsSep "." [ (lib.concatStringsSep "-" [ pkgName site phase ]) "tar" "gz" ];
+  innerTarballName = lib.concatStringsSep "." [
+    (lib.concatStringsSep "-" [ pkgName site phase ])
+    "tar"
+    "gz"
+  ];
 
   # define some utility function for release packing ( code adapted from setup-systemd-units.nix )
-  release-utils = import ./release-utils.nix { inherit lib; pkgs = nPkgs; };
+  release-utils = import ./release-utils.nix {
+    inherit lib;
+    pkgs = nPkgs;
+  };
 
   # the deployment env
-  my-openresty-env = (import ../env/site/${site}/phase/${phase}/env.nix { pkgs = nPkgs; }).env;
+  my-openresty-env =
+    (import ../env/site/${site}/phase/${phase}/env.nix { pkgs = nPkgs; }).env;
 
   # dependent config
-  my-openresty-config = (import ../config/site/${site}/phase/${phase}/config.nix { pkgs = nPkgs; env = my-openresty-env; }).config;
+  my-openresty-config =
+    (import ../config/site/${site}/phase/${phase}/config.nix {
+      pkgs = nPkgs;
+      env = my-openresty-env;
+    }).config;
 
   # the frontend, comment out for now.
-  my-frontend-distributable = import ../frontend/release.nix { inherit site phase; };
+  my-frontend-distributable =
+    import ../frontend/release.nix { inherit site phase; };
 
   # my services dependencies
   # following define the service
@@ -48,7 +61,17 @@ let
       mkdir -p $out/nginx/conf
       for confFile in $src/openresty/nginx/conf/*
       do
-        sed "s/\$OPENRESTY_SERVER_NAME/${my-openresty-config.api-gw.serverName}/g; s/\$OPENRESTY_LISTEN_PORT/${toString my-openresty-config.api-gw.listenPort}/g; s/\$OPENRESTY_RESOLVER/${my-openresty-config.api-gw.resolver}/g; s/\$OPENRESTY_RUN_DIR/${lib.strings.escape ["/"] my-openresty-env.api-gw.runDir}/g; s/\$OPENRESTY_LOG_DIR/${lib.strings.escape ["/"] my-openresty-config.api-gw.logDir}/g; s/\$OPENRESTY_CACHE_DIR/${lib.strings.escape ["/"] my-openresty-config.api-gw.cacheDir}/g; s/\$OPENRESTY_UPLOAD_MAX_SIZE/${toString my-openresty-config.api-gw.uploadMaxSize}/g" $confFile > $out/nginx/conf/$(basename $confFile)
+        sed "s/\$OPENRESTY_SERVER_NAME/${my-openresty-config.api-gw.serverName}/g; s/\$OPENRESTY_LISTEN_PORT/${
+          toString my-openresty-config.api-gw.listenPort
+        }/g; s/\$OPENRESTY_RESOLVER/${my-openresty-config.api-gw.resolver}/g; s/\$OPENRESTY_RUN_DIR/${
+          lib.strings.escape [ "/" ] my-openresty-env.api-gw.runDir
+        }/g; s/\$OPENRESTY_LOG_DIR/${
+          lib.strings.escape [ "/" ] my-openresty-config.api-gw.logDir
+        }/g; s/\$OPENRESTY_CACHE_DIR/${
+          lib.strings.escape [ "/" ] my-openresty-config.api-gw.cacheDir
+        }/g; s/\$OPENRESTY_UPLOAD_MAX_SIZE/${
+          toString my-openresty-config.api-gw.uploadMaxSize
+        }/g" $confFile > $out/nginx/conf/$(basename $confFile)
       done
 
       ln -s $out/lua $out/lualib/user_code
@@ -64,7 +87,9 @@ let
         echo "export DB_SCHEMA=${my-openresty-config.db.apiSchema}"
         echo "export JWT_SECRET=${my-openresty-config.db.jwtSecret}"
         echo "export POSTGREST_HOST=${my-openresty-config.db-gw.server-host}"
-        echo "export POSTGREST_PORT=${toString my-openresty-config.db-gw.server-port}"
+        echo "export POSTGREST_PORT=${
+          toString my-openresty-config.db-gw.server-port
+        }"
         echo "export OPENRESTY_DOC_ROOT=$out/nginx/web"
         echo "export OPENRESTY_UPLOAD_HOME=${my-openresty-config.api-gw.uploadHome}"
       }  > $out/env.export
@@ -92,67 +117,94 @@ let
   };
 
   # following define the service
-  my-openresty-service = { lib, pkgs, config, ... }:
-    {
-      options = lib.attrsets.setAttrByPath [ "services" pkgName ] {
-          enable = lib.mkOption {
-            default = true;
-            type = lib.types.bool;
-            description = "enable to generate a config to start the service";
-          };
-          # add extra options here, if any
-        };
-      config = lib.mkIf (lib.attrsets.getAttrFromPath [ pkgName "enable" ] config.services)
-        (lib.attrsets.setAttrByPath [ "systemd" "services" pkgName ] {
-          wantedBy = [ "multi-user.target" ];
-          after = [ "network.target" ];
-          description = "${pkgName} service";
-          serviceConfig = {
-            Type = "forking";
-            ExecStartPre = ''
-              ${my-openresty-bin-sh}/bin/${my-openresty-bin-sh.name} -e stderr -t -q -g "daemon on; master_process on;"
-            '';
-            ExecStart = ''
-              ${my-openresty-bin-sh}/bin/${my-openresty-bin-sh.name} -e stderr -g "daemon on; master_process on;"
-            '';
-            ExecReload = ''
-              ${my-openresty-bin-sh}/bin/${my-openresty-bin-sh.name} -e stderr -g "daemon on; master_process on;" -s reload
-            '';
-            ExecStop = "${my-openresty-bin-sh}/bin/${my-openresty-bin-sh.name} -e stderr -s stop";
-            Restart = "on-failure";
-          };
-        });
+  my-openresty-service = { lib, pkgs, config, ... }: {
+    options = lib.attrsets.setAttrByPath [ "services" pkgName ] {
+      enable = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
+        description = "enable to generate a config to start the service";
+      };
+      # add extra options here, if any
     };
+    config = lib.mkIf
+      (lib.attrsets.getAttrFromPath [ pkgName "enable" ] config.services)
+      (lib.attrsets.setAttrByPath [ "systemd" "services" pkgName ] {
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+        description = "${pkgName} service";
+        serviceConfig = {
+          Type = "forking";
+          ExecStartPre = ''
+            ${my-openresty-bin-sh}/bin/${my-openresty-bin-sh.name} -e stderr -t -q -g "daemon on; master_process on;"
+          '';
+          ExecStart = ''
+            ${my-openresty-bin-sh}/bin/${my-openresty-bin-sh.name} -e stderr -g "daemon on; master_process on;"
+          '';
+          ExecReload = ''
+            ${my-openresty-bin-sh}/bin/${my-openresty-bin-sh.name} -e stderr -g "daemon on; master_process on;" -s reload
+          '';
+          ExecStop =
+            "${my-openresty-bin-sh}/bin/${my-openresty-bin-sh.name} -e stderr -s stop";
+          Restart = "on-failure";
+        };
+      });
+  };
 
   serviceNameKey = lib.concatStringsSep "." [ pkgName "service" ];
-  serviceNameUnit = lib.attrsets.setAttrByPath [ serviceNameKey ] mk-my-openresty-service-unit;
+  serviceNameUnit =
+    lib.attrsets.setAttrByPath [ serviceNameKey ] mk-my-openresty-service-unit;
 
   mk-my-openresty-service-unit = nPkgs.writeText serviceNameKey
-    (lib.attrsets.getAttrFromPath [ "config" "systemd" "units" serviceNameKey "text" ] (nPkgs.nixos ({ lib, pkgs, config, ... }: {
-      imports = [ my-openresty-service ];
-    })));
+    (lib.attrsets.getAttrFromPath [
+      "config"
+      "systemd"
+      "units"
+      serviceNameKey
+      "text"
+    ] (nPkgs.nixos
+      ({ lib, pkgs, config, ... }: { imports = [ my-openresty-service ]; })));
 
 in rec {
   inherit nativePkgs pkgs my-openresty-config;
 
-  mk-my-openresty-service-systemd-setup-or-bin-sh = if my-openresty-env.api-gw.isSystemdService then
-    (nPkgs.setupSystemdUnits {
-      namespace = pkgName;
-      units = serviceNameUnit;
-    })
-  else
-    my-openresty-bin-sh;
+  mk-my-openresty-service-systemd-setup-or-bin-sh =
+    if my-openresty-env.api-gw.isSystemdService then
+      (nPkgs.setupSystemdUnits {
+        namespace = pkgName;
+        units = serviceNameUnit;
+      })
+    else
+      my-openresty-bin-sh;
 
-  mk-my-openresty-reference = nPkgs.writeReferencesToFile mk-my-openresty-service-systemd-setup-or-bin-sh;
+  mk-my-openresty-service-systemd-unsetup-or-bin-sh =
+    if my-openresty-env.api-gw.isSystemdService then
+      (release-utils.unsetup-systemd-service {
+        namespace = pkgName;
+        units = serviceNameUnit;
+      })
+    else
+      { };
+  # following derivation just to make sure the setup and unsetup will
+  # be packed into the distribute tarball.
+  setup-and-unsetup-or-bin-sh = nPkgs.symlinkJoin {
+    name = "my-openresty-setup-and-unsetup";
+    paths = [
+      mk-my-openresty-service-systemd-setup-or-bin-sh
+      mk-my-openresty-service-systemd-unsetup-or-bin-sh
+    ];
+  };
+
+  mk-my-openresty-reference =
+    nPkgs.writeReferencesToFile setup-and-unsetup-or-bin-sh;
   mk-my-openresty-deploy-sh = release-utils.mk-deploy-sh {
     env = my-openresty-env.api-gw;
-    payloadPath =  mk-my-openresty-service-systemd-setup-or-bin-sh;
+    payloadPath = setup-and-unsetup-or-bin-sh;
     inherit innerTarballName;
     execName = "openresty";
   };
   mk-my-openresty-cleanup-sh = release-utils.mk-cleanup-sh {
     env = my-openresty-env.api-gw;
-    payloadPath =  mk-my-openresty-service-systemd-setup-or-bin-sh;
+    payloadPath = setup-and-unsetup-or-bin-sh;
     inherit innerTarballName;
     execName = "openresty";
   };
--- db-gw/release.nix
+++ db-gw/release.nix
@@ -1,7 +1,7 @@
 { nativePkgs ? import ./default.nix { }, # the native package set
 pkgs ? import ./cross-build.nix { }
 , # the package set for corss build, we're especially interested in the fully static binary
-site , # the site for release, the binary would deploy to it finally
+site, # the site for release, the binary would deploy to it finally
 phase, # the phase for release, must be "local", "test" and "production"
 }:
 let
@@ -9,23 +9,35 @@ let
   sPkgs = pkgs.x86-musl64; # for the fully static build
   lib = nPkgs.lib; # lib functions from the native package set
   pkgName = "my-postgrest";
-  innerTarballName = lib.concatStringsSep "." [ (lib.concatStringsSep "-" [ pkgName site phase ]) "tar" "gz" ];
+  innerTarballName = lib.concatStringsSep "." [
+    (lib.concatStringsSep "-" [ pkgName site phase ])
+    "tar"
+    "gz"
+  ];
 
   # define some utility function for release packing ( code adapted from setup-systemd-units.nix )
-  release-utils = import ./release-utils.nix { inherit lib; pkgs = nPkgs; };
+  release-utils = import ./release-utils.nix {
+    inherit lib;
+    pkgs = nPkgs;
+  };
 
   # the deployment env
-  my-postgrest-env = (import ../env/site/${site}/phase/${phase}/env.nix { pkgs = nPkgs; }).env;
+  my-postgrest-env =
+    (import ../env/site/${site}/phase/${phase}/env.nix { pkgs = nPkgs; }).env;
 
   # dependent config
-  my-postgrest-config = (import ../config/site/${site}/phase/${phase}/config.nix { pkgs = nPkgs; env = my-postgrest-env; }).config;
+  my-postgrest-config =
+    (import ../config/site/${site}/phase/${phase}/config.nix {
+      pkgs = nPkgs;
+      env = my-postgrest-env;
+    }).config;
 
   # my services dependencies
   # following define the service
   my-postgrest-config-kv = nPkgs.writeTextFile {
     name = lib.concatStringsSep "-" [ pkgName "config" ];
     # generate the key = value format config, refer to the lib.generators for other formats
-    text = (lib.generators.toKeyValue {}) my-postgrest-config.db-gw;
+    text = (lib.generators.toKeyValue { }) my-postgrest-config.db-gw;
   };
 
   # my services dependencies
@@ -41,58 +53,84 @@ let
   my-postgrest-service = { lib, pkgs, config, ... }:
     let cfg = config.services.my-postgrest;
     in {
-      options = lib.attrsets.setAttrByPath [ "services" pkgName ]
-        {
-          enable = lib.mkOption {
-            default = true;
-            type = lib.types.bool;
-            description = "enable to generate a config to start the service";
-          };
-          # add extra options here, if any
+      options = lib.attrsets.setAttrByPath [ "services" pkgName ] {
+        enable = lib.mkOption {
+          default = true;
+          type = lib.types.bool;
+          description = "enable to generate a config to start the service";
         };
-      config = lib.mkIf cfg.enable (lib.attrsets.setAttrByPath [ "systemd" "services" pkgName ]
-        {
+        # add extra options here, if any
+      };
+      config = lib.mkIf cfg.enable
+        (lib.attrsets.setAttrByPath [ "systemd" "services" pkgName ] {
           wantedBy = [ "multi-user.target" ];
           after = [ "network.target" ];
           description = "my postgrest service";
           serviceConfig = {
             Type = "simple";
             User = "${my-postgrest-env.db-gw.processUser}";
-            ExecStart = "${my-postgrest-bin-sh}/bin/${my-postgrest-bin-sh.name}";
+            ExecStart =
+              "${my-postgrest-bin-sh}/bin/${my-postgrest-bin-sh.name}";
             Restart = "on-failure";
           };
         });
     };
 
   serviceNameKey = lib.concatStringsSep "." [ pkgName "service" ];
-  serviceNameUnit = lib.attrsets.setAttrByPath [ serviceNameKey ] mk-my-postgrest-service-unit;
+  serviceNameUnit =
+    lib.attrsets.setAttrByPath [ serviceNameKey ] mk-my-postgrest-service-unit;
 
   mk-my-postgrest-service-unit = nPkgs.writeText serviceNameKey
-    (lib.attrsets.getAttrFromPath [ "config" "systemd" "units" serviceNameKey "text" ] (nPkgs.nixos ({ lib, pkgs, config, ... }: {
-      imports = [ my-postgrest-service ];
-    })));
+    (lib.attrsets.getAttrFromPath [
+      "config"
+      "systemd"
+      "units"
+      serviceNameKey
+      "text"
+    ] (nPkgs.nixos
+      ({ lib, pkgs, config, ... }: { imports = [ my-postgrest-service ]; })));
 
 in rec {
   inherit nativePkgs pkgs my-postgrest-config-kv;
 
-  mk-my-postgrest-service-systemd-setup-or-bin-sh = if my-postgrest-env.db-gw.isSystemdService then
-    (nPkgs.setupSystemdUnits {
-      namespace = pkgName;
-      units = serviceNameUnit;
-    })
-  else
-    my-postgrest-bin-sh;
+  mk-my-postgrest-service-systemd-setup-or-bin-sh =
+    if my-postgrest-env.db-gw.isSystemdService then
+      (nPkgs.setupSystemdUnits {
+        namespace = pkgName;
+        units = serviceNameUnit;
+      })
+    else
+      my-postgrest-bin-sh;
 
-  mk-my-postgrest-reference = nPkgs.writeReferencesToFile mk-my-postgrest-service-systemd-setup-or-bin-sh;
+  mk-my-postgrest-service-systemd-unsetup-or-bin-sh =
+    if my-postgrest-env.db-gw.isSystemdService then
+      (release-utils.unsetup-systemd-service {
+        namespace = pkgName;
+        units = serviceNameUnit;
+      })
+    else
+      { };
+  # following derivation just to make sure the setup and unsetup will
+  # be packed into the distribute tarball.
+  setup-and-unsetup-or-bin-sh = nPkgs.symlinkJoin {
+    name = "my-postgrest-setup-and-unsetup";
+    paths = [
+      mk-my-postgrest-service-systemd-setup-or-bin-sh
+      mk-my-postgrest-service-systemd-unsetup-or-bin-sh
+    ];
+  };
+
+  mk-my-postgrest-reference =
+    nPkgs.writeReferencesToFile setup-and-unsetup-or-bin-sh;
   mk-my-postgrest-deploy-sh = release-utils.mk-deploy-sh {
     env = my-postgrest-env.db-gw;
-    payloadPath =  mk-my-postgrest-service-systemd-setup-or-bin-sh;
+    payloadPath = setup-and-unsetup-or-bin-sh;
     inherit innerTarballName;
     execName = "postgrest";
   };
   mk-my-postgrest-cleanup-sh = release-utils.mk-cleanup-sh {
     env = my-postgrest-env.db-gw;
-    payloadPath =  mk-my-postgrest-service-systemd-setup-or-bin-sh;
+    payloadPath = setup-and-unsetup-or-bin-sh;
     inherit innerTarballName;
     execName = "postgrest";
   };
