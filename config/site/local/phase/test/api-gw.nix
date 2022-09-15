269dd9cd9206b6a68db91d70c339f2fc9e70b5d3
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-09-15 22:20:48 +0800

    fix env and config

--- config/site/local/phase/test/api-gw.nix
+++ config/site/local/phase/test/api-gw.nix
@@ -6,9 +6,6 @@
   config = lib.mkIf config.api-gw.enable {
     api-gw = {
       docRoot = "${env.api-gw.runDir}/web";
-      uploadHome = "${env.api-gw.runDir}/upload";
-      logDir = "${env.api-gw.runDir}/log";
-      cacheDir = "${env.api-gw.runDir}/cache";
       serverName = "${env.api-gw.dnsName}";
       listenPort = 80;
       uploadMaxSize = "4096M";
