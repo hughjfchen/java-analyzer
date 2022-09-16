e0d79354ba097fc1e9c467852ba463a1267396bf
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-09-16 22:45:58 +0800

    add pkg-config as buildInputs.

--- db-gw/nix/overlay/haskellPackages.nix
+++ db-gw/nix/overlay/haskellPackages.nix
@@ -3,6 +3,7 @@ prev.haskellPackages.override {
   overrides = selfHP: prevHP: {
     postgresql-libpq = prevHP.postgresql-libpq.overrideAttrs (old: {
       configureFlags = old.configureFlags ++ [ "-f use-pkg-config" ];
+      buildInputs = old.buildInputs ++ [ prev.pkg-config ];
     });
   };
 }
