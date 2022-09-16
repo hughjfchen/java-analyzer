b3008a0b1ab347afc2c399fd24bc20158e0c8244
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-09-16 22:56:52 +0800

    do not know change what.

--- db-gw/nix/overlay/haskellPackages.nix
+++ db-gw/nix/overlay/haskellPackages.nix
@@ -3,7 +3,8 @@ prev.haskellPackages.override {
   overrides = selfHP: prevHP: {
     postgresql-libpq = prevHP.postgresql-libpq.overrideAttrs (old: {
       configureFlags = old.configureFlags ++ [ "-f use-pkg-config" ];
-      buildInputs = old.buildInputs ++ [ prev.pkg-config ];
+      buildInputs = [ prev.pkg-config ] ++ old.buildInputs;
+      nativeBuildInputs = old.nativeBuildInputs ++ [ prev.pkg-config ];
     });
   };
 }
