931e519da34f9edcf19339d4ac5620824d6f1292
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-09-17 07:07:28 +0800

    override postgrest within haskellPackages.

--- db-gw/nix/overlay/haskellPackages.nix
+++ db-gw/nix/overlay/haskellPackages.nix
@@ -6,5 +6,19 @@ prev.haskellPackages.override {
       buildInputs = [ prev.pkg-config ] ++ old.buildInputs;
       nativeBuildInputs = old.nativeBuildInputs ++ [ prev.pkg-config ];
     });
+    postgrest = prevHP.postgrest.overrideAttrs (old: {
+      configureFlags = (if prev.stdenv.hostPlatform.isMusl then
+        old.configureFlags ++ [
+          "--ghc-option=-optl=-lssl"
+          "--ghc-option=-optl=-lcrypto"
+          "--ghc-option=-optl=-L${prev.openssl.out}/lib"
+          "--ghc-option=-optl=-lpgcommon"
+          "--ghc-option=-optl=-lpgport"
+          "--ghc-option=-optl=-L${prev.postgresql.out}/lib"
+        ]
+      else
+        old.configureFlags);
+
+    });
   };
 }
