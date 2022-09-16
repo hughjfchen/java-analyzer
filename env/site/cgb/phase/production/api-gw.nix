58deba446543de6b1fe93cf61a5410d8b99b500b
Hugh JF Chen <hugh.jf.chen@gmail.com>
2022-09-16 08:39:50 +0800

    fix again.

--- env/site/cgb/phase/production/api-gw.nix
+++ env/site/cgb/phase/production/api-gw.nix
@@ -4,7 +4,7 @@
   config = {
     api-gw = rec {
       hostName = "a-zdhyw-app05";
-      dnsName = "a-zdhyw-app05";
+      dnsName = "azdhywapp05";
       ipAddress = "21.2.109.74";
       processUser = "jaapiuser";
       isSystemdService = true;
